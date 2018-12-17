fdc81345739eb63361e49bd174b3ae8d3c666274
Bijan Parvin <bparvin12@gmail.com>
2018-12-16 20:02:47 -0800

    mad libs project

---
+++ index.js
@@ -0,0 +1,62 @@
+var inquirer = require("inquirer");
+
+function MadLib (nouns,adjectives,verbs){
+    this.nouns = nouns;
+    this.adjectives = adjectives;
+    this.verbs = verbs; 
+    this.story = "Once upon a time [noun] there was a [noun] who [verb]. The [noun] was a [adjective] [noun] who lived under a [noun]."
+
+};
+
+var nouns = [];
+var advjectives = [];
+var verbs = [];
+var loop = 0 
+
+var getWords = function (loop) {
+    if (loop < 5) {
+        inquirer.prompt({
+            name: "noun",
+            message: "Enter a noun: "
+        }).then(function(answers){
+            nouns.push(answers.noun)
+            loop++; 
+            getWords(loop);
+        });
+    }
+    if (loop >=5 && loop < 6) {
+        inquirer.prompt({
+            name: "adjective",
+            message: "Enter an adjective: "
+        }).then(function(answers) {
+            adjectives.push(answers.adjective);
+            loop++
+            getWords(loop);
+        })
+    }
+    if (loop >= 6) {
+        inquirer.prompt({
+            name: "verb",
+            message: "Enter a verb: "
+        }).then(function(answers){
+            verbs.push(answers.verb);
+            var MadLib = new MadLib(nouns,advjectives,verbs);
+            console.log(MadLib);
+        })
+    }
+};
+
+var constructStory = function(completeObject){
+    for (var i = 0; i<5; i++) {
+        completeObject.story = completeObject.story.replace("[noun]",completeObject.nouns[i]);
+    }
+    for (var i=0; i<1; i++){
+        completeObject.story = completeObject.story.replace("[adjective]",completeObject.adjectives[i]);
+    }
+    for (var i=0;i<1;i++){
+        completeObject.story = completeObject.story.replace("[verb]",completeObject.verbs[i]);
+    }
+    console.log(completeObject.story);
+};
+
+getWords(loop);
